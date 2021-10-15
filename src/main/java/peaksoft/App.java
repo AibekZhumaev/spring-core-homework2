8b01113000b13f04e6a65c05ec0a657db231062c
AibekZhumaev <90904341+AibekZhumaev@users.noreply.github.com>
2021-10-15 13:25:17 +0600

    add Class Cat

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -10,5 +10,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1 == cat2);
+        System.out.println("Cat1" + cat1.hashCode());
+        System.out.println("Cat2" + cat2.hashCode());
     }
 }
---
+++ src/main/java/peaksoft/Cat.java
@@ -0,0 +1,10 @@
+package peaksoft;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+    public Cat() {
+    }
+}
